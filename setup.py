11753c434eca1ab7e2e85ab29ce305c72631c501
Pratyush Tripathy <pratkrt@gmail.com>
2021-07-16 10:19:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+packages=setuptools.find_packages()
+packages = packages + ['numpy', 'scikit-learn']
+
 setuptools.setup(
     name="pyrsgis",
-    version="0.3.8",
+    version="0.4.1rc1",
     author="Pratyush Tripathy",
     author_email="pratkrt@gmail.com",
     description="A package to read, process and export GeoTIFFs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pratkrt/Python-for-Remote-Sensing-and-GIS",
-    packages=setuptools.find_packages(),
+    url="https://github.com/PratyushTripathy/pyrsgis",
+    packages=packages,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Development Status :: 5 - Production/Stable",
