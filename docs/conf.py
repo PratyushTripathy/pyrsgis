964ec998f3f5a3dcc95218cb787d3b9ce7e22ac8
Pratyush Tripathy <pratkrt@gmail.com>
2021-07-16 10:26:36 +0530

    docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ html_theme_options = {
 
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-html_title = "%s v%s Documentation" % ('pyrsgis', pyrsgis.__version__)
+html_title = "%s v%s Documentation" % ('pyrsgis', version)
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
